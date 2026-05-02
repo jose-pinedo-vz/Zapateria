552c7d0d24d02aef6712721f42b7fd44c448e33c
jose pinedo <josepinedov.24826@gmail.com>
2026-05-02 16:38:47 -0600

    base de datos modificada

--- BancoAsociado.sql
+++ BancoAsociado.sql
@@ -16,11 +16,17 @@ Sucursal varchar(20),
 foreign key(Sucursal) references Sucursal(Nombre_Sucursal),
 foreign key(ClaveCuenta) references Cliente(ClaveCuenta));
 
-create table Cliente
-(ClaveCuenta varchar(5)primary key,
-NombreCliente varchar(10),
-ApellidoM varchar(10),
-ApeelidoP varchar(10));
+CREATE TABLE Clientes (
+    clave CHAR(5) PRIMARY KEY,
+    Nombre VARCHAR(50),        
+    ApellidoP VARCHAR(50),     
+    Email VARCHAR(100),       
+    Direccion VARCHAR(200),    
+    telefono BIGINT            
+);
+GO
+
+
 
 create table Transaccion
 (ClaveDeposito varchar(10)primary key,
@@ -56,3 +62,26 @@ INSERT INTO Cuenta VALUES('tl-02', 500,'Zacatecas Sur');
 
 INSERT INTO Transaccion VALUES('tl-jpv-000','tl-01','tl-02');
 INSERT INTO Deposito VALUES('tl-jpv-000','tl-01',500);
+
+
+
+CREATE TABLE Productos (
+    Clave CHAR(5) PRIMARY KEY, 
+    Modelo VARCHAR(100),
+    Marca VARCHAR(100),  
+    Seccion VARCHAR(100), 
+    Categoria VARCHAR(100)
+);
+
+
+create table Inventario
+(Clave char(5), 
+CantidadProducto int, 
+Talla int, 
+FechaIngreso datetime, 
+Precio DECIMAL(10,2), 
+Color char(100), 
+RutaImagen CHAR(300), 
+
+PRIMARY key(Clave, talla, color), 
+foreign key (Clave) references Productos(Clave))
